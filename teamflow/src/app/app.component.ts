e44e255541ff4f59d91734774b651cfffb070c8b
Francisco <francisco.piqueras.smr@gmail.com>
2025-06-06 16:16:16 +0200

    Primera versión
    Estructura inicial básica terminada, el template HTML por ahora está en el mismo componente

---
+++ teamflow/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template: '<h1>Hola, prueba de estructura</h1>',
+})
+export class AppComponent {
+  protected title = 'TeamFlow';
+}
--- teamflow/src/main.ts
+++ teamflow/src/main.ts
@@ -1,6 +1,10 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./app/app.component";
+// Punto de arranque de la aplicación
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+// bootstrapApplication es un método que se encarga de inicializar
+// la aplicación desde AppComponent
+bootstrapApplication(AppComponent)
+    .catch(err => {
+        throw new Error((err as Error).message);
+    })

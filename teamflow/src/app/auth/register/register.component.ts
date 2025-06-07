0d709bcb557b6a6c53259a9817eb81e4219eb400
Francisco <francisco.piqueras.smr@gmail.com>
2025-06-07 14:05:40 +0200

    Primera versión funcional de CSS
    CSS implementado en su primera versión, faltan transiciones y demás

--- teamflow/src/app/auth/register/register.component.ts
+++ teamflow/src/app/auth/register/register.component.ts
@@ -3,5 +3,6 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'auth-register',
     templateUrl: './register.template.html',
+    styleUrl: './register.styles.css',
 })
 export class RegisterComponent { }

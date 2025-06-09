e8ca11966f7ef529f7e302a45002bfcfc856af2d
Francisco <francisco.piqueras.smr@gmail.com>
2025-06-09 18:51:27 +0200

    Estructura inicial formulario reactivo
    He solucionado un problema que generaba el elemento <Link /> en index.html por la fuente.
    Estructura inicial básica de un formulario reactivo en Angular, aún falta añadir validaciones, etc...

--- teamflow/src/app/auth/register/register.component.ts
+++ teamflow/src/app/auth/register/register.component.ts
@@ -1,10 +1,49 @@
 import { Component } from "@angular/core";
+import { FormGroup, FormControl, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
     selector: 'auth-register',
+    imports: [
+        ReactiveFormsModule,
+    ],
     templateUrl: './register.template.html',
     styleUrl: './register.styles.css',
 })
 export class RegisterComponent {
+    // Para cambiar la vista de los overlays y pasar al modo de inicio de sesión
+    // o de registro de nueva cuenta
     rightPanelActive = true;
+
+    // FormGroup para agrupar varias instancias de control de formulario
+    // FormControl para hacer instancias individuales de campos específicos de HTML
+    signUpForm = new FormGroup({
+        name: new FormControl(''),
+        surnames: new FormControl(''),
+        email: new FormControl(''),
+        password: new FormControl(''),
+    });
+
+    signInForm = new FormGroup({
+        email: new FormControl(''),
+        password: new FormControl(''),
+    });
+
+    onSubmitSignInForm() {
+        if (this.signInForm.valid) {
+            // Objeto con los campos del formulario de inicio de sesión
+            const signInObject = this.signInForm.value;
+        } else {
+            // TODO: Mejorar esto
+        }
+    };
+
+    onSubmitSignUpForm() {
+        if (this.signUpForm.valid) {
+            // Objeto con los campos del formulario de registro
+            const signUpFormObject = this.signUpForm.value;
+        } else {
+            // TODO: Mejorar esto
+        }
+    }
+
 }

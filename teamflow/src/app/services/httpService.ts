2b839df526ee266afe529e9c528881f0af035920
Francisco <francisco.piqueras.smr@gmail.com>
2025-06-10 18:28:17 +0200

    Pequeño cambio en el servicio HTTP

--- teamflow/src/app/services/httpService.ts
+++ teamflow/src/app/services/httpService.ts
@@ -3,5 +3,8 @@ import { inject, Injectable } from "@angular/core";
 
 @Injectable({providedIn: 'root'})
 export class HttpService {
+    // Servicio para hacer solicitudes HTTP al servidor
     private http = inject(HttpClient);
+
+    
 };

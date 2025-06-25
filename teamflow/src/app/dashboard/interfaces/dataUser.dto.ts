431a36cd56cad9004b04fd7f230da690de4d7f1e
Francisco <francisco.piqueras.smr@gmail.com>
2025-06-25 12:12:36 +0200

    DTO de respuesta de usuario

---
+++ teamflow/src/app/dashboard/interfaces/dataUser.dto.ts
@@ -0,0 +1,7 @@
+export interface DataUserResponse {
+    usuario_id: number;
+    correo_usuario: string;
+    contraseña_usuario: string;
+    nombre_usuario: string;
+    apellidos_usuario: string;
+};
